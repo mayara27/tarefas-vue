8218df868edaee99c6d1b08f214fa41cd5f1fdf9
mayara.fernandes <mayara.fernandes@locaweb.com.br>
2021-10-18 16:29:20 -0300

    to do app

---
+++ src/components/NewTask.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="new-task">
+      <input class="form-element" v-model="name" @keydown.enter="add" 
+             type="text" placeholder="Adicione uma tarefa">
+      <button @click="add" class="form-element">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            name: ''
+        }
+    },
+
+    methods: {
+        add() {
+            this.$emit('taskAdded', { name: this.name })
+            this.name = ''
+        }
+    }
+}
+</script>
+
+<style>
+    .new-task{
+        margin: 35px;
+    }
+
+    .form-element{
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid white;
+        padding: 5px 10px 8px;
+    }
+
+    input.form-element{
+        width: 500px;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+    }
+
+    button.form-element{
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+        border-left: none;
+        background-color: rgb(0, 179, 250);
+        color: white;
+    }
+</style>
---
+++ src/components/TaskProgress.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="tasks-progress">
+      <span class="progess-value"> {{ progress }}% </span>
+      <div class="progress-bar" :style="{width: progress +'%'}">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: { 
+        progress: { type: Number, default: 0 }
+    }
+}
+</script>
+
+<style>
+
+    .tasks-progress {
+        position: relative;
+        width: 80%;
+        border: 1px solid white;
+        color: white;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .progress-bar {
+        position: absolute;
+        border-radius: 8px;
+        height: 100%;
+        background-color: green;
+        align-self: flex-start;
+    }
+
+    .progess-value {
+        z-index: 1;
+        font-size: 1.5rem;
+    }
+</style>
